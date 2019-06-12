b017073fd43045f9b9dac9f704ba6e09a1c9844e
Erick Tepale <e.tepale@gmail.com>
2019-06-12 16:58:14 -0400

    more tests for CardGame

--- src/test/java/io/zipcoder/casino/Mediators/CardGameTest.java
+++ src/test/java/io/zipcoder/casino/Mediators/CardGameTest.java
@@ -14,32 +14,71 @@ import java.util.ArrayList;
 
 public class CardGameTest extends CardGame{
     private Deck testDeck;
-    private Hand testHand;
+    private ArrayList<Card> testHand;
+
+    public CardGameTest(){
+        super();
+    }
 
     @Before
     public void setup(){
-        testDeck = new Deck();
-        testHand = new Hand();
+        testDeck = super.getDeck();
+        testHand = null;
     }
 
     @Test
     public void testSize(){
         Integer expected = 52;
-        testDeck = super.getDeck();
 
         Assert.assertEquals(expected, testDeck.getDeckSize());
     }
 
     @Test
-    public void drawAndSizeTest(){
+    public void burnTest(){
         Integer expected = 51;
-        //Integer actual = testDeck.getDeckSize();
-        //Assert.assertEquals(expected, actual);
+
+        Deck.burn(1);
+        Integer actual = testDeck.getDeckSize();
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void returnCardsFromDeck1(){
+    public void dealSizeTest1(){
+        //expected
+        Integer expected = 5;
 
+        //action
+        testHand = super.deal(5);
+        Integer size = testHand.size();
 
+        //check
+        Assert.assertEquals(expected, size);
     }
+
+    @Test
+    public void dealSizeTest2(){
+        //expected
+        Integer expected = 25;
+
+        //action
+        testHand = super.deal(25);
+        Integer size = testHand.size();
+
+        //check
+        Assert.assertEquals(expected, size);
+    }
+
+    @Test
+    public void dealSizeTest3(){
+        //expected
+        Integer expected = 52;
+
+        //action
+        testHand = super.deal(52);
+        Integer size = testHand.size();
+
+        //check
+        Assert.assertEquals(expected, size);
+    }
+
 }
